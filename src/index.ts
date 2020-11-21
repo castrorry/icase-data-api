6c2ebcb758f323b820e5f0035f8218e5ac06476d
Rayan <castrorry@Rayan-Macmini.local>
2020-11-21 19:15:11 -0300

    fix indetation

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ App.use(Express.json());
 App.use(Routes);
 
 App.listen(process.env.PORT || 3000, () => {
-	console.log(
-		`Express Application running on port: ${process.env.PORT || 3000}`
-	);
+  console.log(
+    `Express Application running on port: ${process.env.PORT || 3000}`
+  );
 });
