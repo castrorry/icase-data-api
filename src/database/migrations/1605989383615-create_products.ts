a69ab2d9c75e789f2ce1e5865b36caf5690aa49c
Rayan <castrorry@Rayan-Macmini.local>
2020-11-23 09:57:59 -0300

    update indentation

--- src/database/migrations/1605989383615-create_products.ts
+++ src/database/migrations/1605989383615-create_products.ts
@@ -2,76 +2,76 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class createProducts1605989383615 implements MigrationInterface {
 
-	public async up(queryRunner: QueryRunner): Promise<void> {
-		return await queryRunner.createTable(new Table({
-			name: 'products',
-			columns: [
-				{
-					name: 'id',
-					type: 'integer',
-					unsigned: true,
-					isPrimary: true,
-					isGenerated: true,
-					generationStrategy: 'increment'
-				},
-				{
-					name: 'name',
-					type: 'varchar'
-				},
-				{
-					name: 'count',
-					type: 'integer'
-				},
-				{
-					name: 'price',
-					type: 'decimal'
-				},
-				{
-					name: 'stock_price',
-					type: 'decimal'
-				},
-				{
-					name: 'created_at',
-					type: 'timestamp',
-					default: 'now()'
-				},
-				{
-					name: 'updated_at',
-					type: 'timestamp',
-					default: 'now()'
-				},
-				{
-					name: 'product_type_id',
-					type: 'integer'
-				},
-				{
-					name: 'category_id',
-					type: 'integer'
-				}
-			],
-			foreignKeys: [
-				{
-					name: 'TypeProduct',
-					columnNames: ['product_type_id'],
-					referencedTableName: 'product_types',
-					referencedColumnNames: ['id'],
-					onUpdate: 'CASCADE',
-					onDelete: 'CASCADE'
-				},
-				{
-					name: 'CategoryProduct',
-					columnNames: ['category_id'],
-					referencedTableName: 'categories',
-					referencedColumnNames: ['id'],
-					onUpdate: 'CASCADE',
-					onDelete: 'CASCADE'
-				}
-			]
-		}));
-	}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    return await queryRunner.createTable(new Table({
+      name: 'products',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: 'increment'
+        },
+        {
+          name: 'name',
+          type: 'varchar'
+        },
+        {
+          name: 'count',
+          type: 'integer'
+        },
+        {
+          name: 'price',
+          type: 'decimal'
+        },
+        {
+          name: 'stock_price',
+          type: 'decimal'
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'now()'
+        },
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default: 'now()'
+        },
+        {
+          name: 'product_type_id',
+          type: 'integer'
+        },
+        {
+          name: 'category_id',
+          type: 'integer'
+        }
+      ],
+      foreignKeys: [
+        {
+          name: 'TypeProduct',
+          columnNames: ['product_type_id'],
+          referencedTableName: 'product_types',
+          referencedColumnNames: ['id'],
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        },
+        {
+          name: 'CategoryProduct',
+          columnNames: ['category_id'],
+          referencedTableName: 'categories',
+          referencedColumnNames: ['id'],
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        }
+      ]
+    }));
+  }
 
-	public async down(queryRunner: QueryRunner): Promise<void> {
-		return await queryRunner.dropTable('products');
-	}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return await queryRunner.dropTable('products');
+  }
 
 }
--- src/database/migrations/1606071920578-create_devices.ts
+++ src/database/migrations/1606071920578-create_devices.ts
@@ -1,52 +1,52 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class createDevices1606071920578 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-      return await queryRunner.createTable(new Table({
-        name: 'devices',
-        columns: [
-          {
-            name: 'id',
-            type: 'integer',
-            unsigned: true,
-            isPrimary: true,
-            isGenerated: true,
-            generationStrategy: 'increment'
-          },
-          {
-            name: 'name',
-            type: 'varchar'
-          },
-          {
-            name: 'model',
-            type: 'varchar',
-            isUnique: true
-          },
-          {
-            name: 'brand',
-            type: 'varchar'
-          },
-          {
-            name: 'year',
-            type: 'varchar'
-          },
-          {
-            name: 'created_at',
-            type: 'timestamp',
-            default: 'now()'
-          },
-          {
-            name: 'updated_at',
-            type: 'timestamp',
-            default: 'now()'
-          }
-        ]
-      }));
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    return await queryRunner.createTable(new Table({
+      name: 'devices',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: 'increment'
+        },
+        {
+          name: 'name',
+          type: 'varchar'
+        },
+        {
+          name: 'model',
+          type: 'varchar',
+          isUnique: true
+        },
+        {
+          name: 'brand',
+          type: 'varchar'
+        },
+        {
+          name: 'year',
+          type: 'varchar'
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'now()'
+        },
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default: 'now()'
+        }
+      ]
+    }));
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-      return await queryRunner.dropTable('devices');
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return await queryRunner.dropTable('devices');
+  }
 
 }
