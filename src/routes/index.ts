8a211cec407b0742db8d4f4af742845af4636c7f
Ryan Castro <castro.rry@gmail.com>
2020-12-25 00:08:42 -0300

    enable all routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import { categoryRoutes } from "./categories.routes";
 
 import { deviceRoutes } from "./devices.routes";
+import { productRoutes } from "./products.routes";
+import { productTypeRoutes } from "./productTypes.routes";
 
 const Routes = Router();
 // Device routes
 Routes.use('/devices', deviceRoutes);
+// category routes
+Routes.use('/categories', categoryRoutes);
+// ProductType routes
+Routes.use('/productTypes', productTypeRoutes);
+// Product routes
+Routes.use('/products', productRoutes);
 
 Routes.get('/', (request, response) => {
   return response.json({
