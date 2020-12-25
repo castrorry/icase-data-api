3fc0c3d90559af3d10b836315a917eb379a8ad54
Ryan Castro <castro.rry@gmail.com>
2020-12-25 01:00:11 -0300

    update category controller

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,37 +1,67 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { Category } from "../models/Category";
 
 export default {
-  create: (request: Request, response: Response) => {
+  create: async (request: Request, response: Response) => {
     try {
-      return response.status(201).json({ ok: true });
+      const { name } = request.body;
+
+      const categoryRepository = getRepository(Category);
+      const category = categoryRepository.create({ name });
+      const categoryStored = await categoryRepository.save(category);
+
+      return response.status(201).json(categoryStored);
     } catch ({ message }) {
       return response.status(400).json({ message });
     }
   },
-  index: (request: Request, response: Response) => {
+  index: async (request: Request, response: Response) => {
     try {
-      return response.status(200).json({ ok: true });
+      const categoryRepository = getRepository(Category);
+      const categories = await categoryRepository.find();
+
+      return response.status(200).json(categories);
     } catch ({ message }) {
       return response.status(400).json({ message });
     }
   },
-  show: (request: Request, response: Response) => {
+  show: async (request: Request, response: Response) => {
     try {
-      return response.status(200).json({ ok: true });
+      const {id: category_id} = request.params;
+
+      const categoryRepository = getRepository(Category);
+      const category = await categoryRepository.findOneOrFail(category_id);
+
+      return response.status(200).json(category);
     } catch ({ message }) {
       return response.status(400).json({ message });
     }
   },
-  update: (request: Request, response: Response) => {
+  update: async (request: Request, response: Response) => {
     try {
-      return response.status(201).json({ ok: true });
+      const { name } = request.body;
+      const {id: category_id} = request.params;
+
+      const categoryRepository = getRepository(Category);
+      const categoryStored = await categoryRepository.save({
+        ...await categoryRepository.findOneOrFail(category_id),
+        name
+      });
+
+      return response.status(201).json(categoryStored);
     } catch ({ message }) {
       return response.status(400).json({ message });
     }
   },
-  delete: (request: Request, response: Response) => {
+  delete: async (request: Request, response: Response) => {
     try {
-      return response.status(200).json({ ok: true });
+      const {id: category_id} = request.params;
+
+      const categoryRepository = getRepository(Category);
+      const categoryDeleted = await categoryRepository.delete(category_id);
+
+      return response.status(200).json(categoryDeleted);
     } catch ({ message }) {
       return response.status(400).json({ message });
     }
