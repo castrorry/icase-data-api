8b4757ec1508c5a8e60a03544b56f39d32bd608a
Rayan Castro <castro.rry@gmail.com>
2021-01-06 20:55:24 -0300

    implement products controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,37 +1,85 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { Product } from "../models/Product";
 
 export default {
-  create: (request: Request, response: Response) => {
+  create: async (request: Request, response: Response) => {
     try {
-      return response.status(201).json({ ok: true });
+      const {
+        name,
+        category_id,
+        product_type_id,
+        count,
+        price,
+        stock_price
+      } = request.body;
+
+      const productRepository = getRepository(Product);
+      const product = productRepository.create({
+        name,
+        category_id,
+        product_type_id,
+        count,
+        price,
+        stock_price
+      });
+      const productStored = await productRepository.save(product);
+
+      return response.status(201).json(productStored);
     } catch ({ message }) {
       return response.status(400).json({ message });
     }
   },
-  index: (request: Request, response: Response) => {
+  index: async (request: Request, response: Response) => {
     try {
-      return response.status(200).json({ ok: true });
+      const productRepository = getRepository(Product);
+      const products = await productRepository.find({
+        relations: ['category', 'product_type']
+      });
+
+      return response.status(200).json(products);
     } catch ({ message }) {
       return response.status(400).json({ message });
     }
   },
-  show: (request: Request, response: Response) => {
+  show: async (request: Request, response: Response) => {
     try {
-      return response.status(200).json({ ok: true });
+      const { id: product_id } = request.params;
+
+      const productRepository = getRepository(Product);
+      const product = await productRepository.findOneOrFail(product_id, {
+        relations: ['category', 'product_type']
+      });
+
+      return response.status(200).json(product);
     } catch ({ message }) {
       return response.status(400).json({ message });
     }
   },
-  update: (request: Request, response: Response) => {
+  update: async (request: Request, response: Response) => {
     try {
-      return response.status(201).json({ ok: true });
+      const productDTO = request.body;
+      const { id: product_id } = request.params;
+
+      const productRepository = getRepository(Product);
+      const productUpdated = productRepository.save({
+        ...await productRepository.findOneOrFail(product_id),
+        ...productDTO
+      });
+
+      return response.status(201).json(productUpdated);
     } catch ({ message }) {
       return response.status(400).json({ message });
     }
   },
-  delete: (request: Request, response: Response) => {
+  delete: async (request: Request, response: Response) => {
     try {
-      return response.status(200).json({ ok: true });
+      const {id: product_id} = request.params;
+
+      const productRepository = getRepository(Product);
+      const productDeleted = await productRepository.delete(product_id);
+
+      return response.status(200).json(productDeleted);
     } catch ({ message }) {
       return response.status(400).json({ message });
     }
